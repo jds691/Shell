33daad158a4a3e7e609e9587a911a313ddc5645f
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-03-15 15:54:08 +0000

    Start Shell immediately when constructing Host
    Also use PowerShell instead of CMD

--- Shell.Core/ShellHost.cs
+++ Shell.Core/ShellHost.cs
@@ -4,36 +4,45 @@ namespace Shell.Core;
 
 public class ShellHost
 {
-    private Process? _currentProcess;
+    private Process _currentProcess;
+    
+    public event EventHandler<string?> OutputReceived; 
+    public event EventHandler<string?> ErrorReceived;
+
+    public ShellHost()
+    {
+        _currentProcess = new Process();
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        
+        startInfo.FileName = "powershell.exe";
+        startInfo.UseShellExecute = false;
+        startInfo.CreateNoWindow = true;
+        startInfo.WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+        
+        startInfo.RedirectStandardInput = true;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+        
+        _currentProcess.StartInfo = startInfo;
+
+        _currentProcess.OutputDataReceived += (_, args) =>
+        {
+            OutputReceived?.Invoke(this, args.Data);
+        };
+        _currentProcess.ErrorDataReceived += (_, args) =>
+        {
+            ErrorReceived?.Invoke(this, args.Data);
+        };
+        _currentProcess.EnableRaisingEvents = true;
+        
+        _currentProcess.Start();   
+            
+        _currentProcess.BeginOutputReadLine();
+        _currentProcess.BeginErrorReadLine();
+    }
     
     public void RunCommand(string command)
     {
-        if (_currentProcess == null)
-        {
-            _currentProcess = new Process();
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-        
-            startInfo.FileName = "cmd.exe";
-            startInfo.UseShellExecute = false;
-            startInfo.CreateNoWindow = true;
-            startInfo.WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        
-            startInfo.RedirectStandardInput = true;
-            startInfo.RedirectStandardOutput = true;
-            startInfo.RedirectStandardError = true;
-        
-            _currentProcess.StartInfo = startInfo;
-
-            _currentProcess.OutputDataReceived += (_, args) => { Console.WriteLine(args.Data); };
-            _currentProcess.ErrorDataReceived += (_, args) => { Console.WriteLine(args.Data); };
-            _currentProcess.EnableRaisingEvents = true;
-        
-            _currentProcess.Start();   
-            
-            _currentProcess.BeginOutputReadLine();
-            _currentProcess.BeginErrorReadLine();
-        }
-        
         _currentProcess.StandardInput.WriteLine(command);
         _currentProcess.StandardInput.Flush();
     }
